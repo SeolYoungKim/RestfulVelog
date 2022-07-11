8b1510e0384fd559ab789ce9e2f5d23a11327dfa
SeolYoungKim <nasur4da@gmail.com>
2022-07-11 21:35:30 +0900

    git init

---
+++ src/main/java/ToyProject/RestfulVelog/RestfulVelogApplication.java
@@ -0,0 +1,13 @@
+package ToyProject.RestfulVelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulVelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulVelogApplication.class, args);
+	}
+
+}
