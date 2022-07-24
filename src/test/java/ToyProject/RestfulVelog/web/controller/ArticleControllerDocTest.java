973e945ee6ab7706da8533391b90bca887345e5f
SeolYoungKim <nasur4da@gmail.com>
2022-07-24 20:03:13 +0900

    요청 및 응답에 대한 문서화

--- src/test/java/ToyProject/RestfulVelog/web/controller/ArticleControllerDocTest.java
+++ src/test/java/ToyProject/RestfulVelog/web/controller/ArticleControllerDocTest.java
@@ -2,40 +2,41 @@ package ToyProject.RestfulVelog.web.controller;
 
 import ToyProject.RestfulVelog.domain.Article;
 import ToyProject.RestfulVelog.repository.ArticleRepository;
-import org.junit.jupiter.api.BeforeEach;
+import ToyProject.RestfulVelog.web.request.AddArticle;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.velog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class ArticleControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ArticleRepository articleRepository;
 
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("hello print")
@@ -57,10 +58,51 @@ public class ArticleControllerDocTest {
 
         articleRepository.save(article);
 
-        this.mockMvc.perform(get("/article/{id}", 1L)
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/article/{id}", 1L)
                         .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("readArticle"));
+                .andDo(document("readArticle",
+                        pathParameters(
+                                parameterWithName("id").description("게시글의 ID 입니다.")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글의 ID 입니다."),
+                                fieldWithPath("title").description("게시글의 제목 입니다."),
+                                fieldWithPath("text").description("게시글의 내용 입니다.")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("write article")
+    void test3() throws Exception {
+
+        AddArticle addArticle = AddArticle.builder()
+                .title("제목1")
+                .text("내용1")
+                .build();
+
+        String json = objectMapper.writeValueAsString(addArticle);
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/write")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("addArticle",
+                        requestFields(
+                                fieldWithPath("title").description("등록하고자 하는 제목입니다."),
+                                fieldWithPath("text").description("등록하고자 하는 내용입니다.")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글의 ID 입니다."),
+                                fieldWithPath("title").description("게시글의 제목입니다."),
+                                fieldWithPath("text").description("게시글의 내용입니다.")
+                        )
+                ));
     }
 }
+
+
